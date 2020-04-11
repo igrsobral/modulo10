ff0fcfee0e0e1df5d2a52af19b67e4dcac06475b
igorWebMASTER <igor_1917@hotmail.com>
2020-04-11 00:02:04 -0300

    start of application

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+import Background from '~/components/Background';
+
+export default function SignUp() {
+  return (
+    <Background>
+      <Text>Faça o login2w\1</Text>);
+    </Background>
+  );
+}
